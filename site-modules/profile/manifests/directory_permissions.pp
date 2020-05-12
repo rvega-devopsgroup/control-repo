ed3faff3acbd762f55ab2e4b0c42c2801dd8087f
Roberto Vega <roberto.vega@devopsgroup.com>
2020-05-12 14:35:07 +0100

    adding an ACL to set permissions on the new directory

--- site-modules/profile/manifests/directory_permissions.pp
+++ site-modules/profile/manifests/directory_permissions.pp
@@ -4,4 +4,12 @@ class profile::directory_permissions {
     owner  => 'student1',
     group  => 'Students',
   }
+
+  acl { 'C:/directory':
+    purge => true,
+    permissions => [
+      { identity => 'student1', rights => ['full'] },
+      { identity => 'Students', rights => ['read'] },
+    ],
+  }
 }
