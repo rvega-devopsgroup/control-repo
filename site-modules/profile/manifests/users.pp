f9abf258ec9e1d19433666f8a4fbd290bc6a63fe
Roberto Vega <roberto.vega@devopsgroup.com>
2020-05-12 12:03:25 +0100

    still with issues - managing the group from the group resource

--- site-modules/profile/manifests/users.pp
+++ site-modules/profile/manifests/users.pp
@@ -1,13 +1,13 @@
 class profile::users {
   user { 'student1':
     ensure => present,
-    groups => ['Users', 'student1'],
+    groups => ['Users'],
     password => 'student1',
     managehome => true,
   }
 
   group { 'student1':
     ensure => present,
-    before => User['student1'],
+    members => ['student1'],
   }
 }
