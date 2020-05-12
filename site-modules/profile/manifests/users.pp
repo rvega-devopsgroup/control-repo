b6f5eb42bcec3cf2b7e679d649c486d283c74c66
Roberto Vega <roberto.vega@devopsgroup.com>
2020-05-12 11:50:12 +0100

    creating a new group

--- site-modules/profile/manifests/users.pp
+++ site-modules/profile/manifests/users.pp
@@ -1,5 +1,11 @@
 class profile::users {
   user { 'student1':
     ensure => present,
+    groups => 'student1',
+  }
+
+  group { 'student1':
+    ensure => present,
+    before => User['student1'],
   }
 }
