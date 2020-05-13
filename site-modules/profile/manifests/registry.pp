ab66379f6bdf1ae0031bb836cae34879d22841cc
Roberto Vega <roberto.vega@devopsgroup.com>
2020-05-13 16:56:00 +0100

    linting registry.pp file

--- site-modules/profile/manifests/registry.pp
+++ site-modules/profile/manifests/registry.pp
@@ -6,18 +6,18 @@ class profile::registry {
   registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows NT\Reliability\ShutdownReasonOn':
     ensure => present,
     type   => string,
-    data   => "1",
+    data   => '1',
   }
 
   registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows NT\Reliability\ShutdownReasonUI':
     ensure => present,
     type   => string,
-    data   => "1",
-  } 
+    data   => '1',
+  }
 
   registry_value { 'HKLM\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}\IsInstalled':
     ensure => present,
     type   => string,
-    data   => "1",
+    data   => '1',
   }
 }
