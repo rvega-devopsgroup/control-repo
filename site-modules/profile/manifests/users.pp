33b355292d7ba7f26d7dd652d851bc4fc6f90ba4
Roberto Vega <roberto.vega@devopsgroup.com>
2020-05-13 16:12:42 +0100

    adding  domain prefix to student1 user

--- site-modules/profile/manifests/users.pp
+++ site-modules/profile/manifests/users.pp
@@ -13,7 +13,7 @@ class profile::users {
 
   local_security_policy { 'Log on as a service':
     ensure       => present,
-    policy_value => 'student1',
+    policy_value => 'DOMAIN\student1',
     require      => User['student1'],
   } 
 }
