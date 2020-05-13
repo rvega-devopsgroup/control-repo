34af1b2f4bb5667e45800aedfeae63597b635c87
Roberto Vega <roberto.vega@devopsgroup.com>
2020-05-13 16:33:41 +0100

    creating parent registry key

--- site-modules/profile/manifests/registry.pp
+++ site-modules/profile/manifests/registry.pp
@@ -1,11 +1,15 @@
 class profile::registry {
+  registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows NT\Reliability':
+    ensure => present,
+  }
+
   registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows NT\Reliability\ShutdownReasonOn':
     ensure => present,
     type   => string,
     data   => "1",
   }
 
-    registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows NT\Reliability\ShutdownReasonUI':
+  registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows NT\Reliability\ShutdownReasonUI':
     ensure => present,
     type   => string,
     data   => "1",
