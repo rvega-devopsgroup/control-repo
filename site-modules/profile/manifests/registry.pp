efac5337d52e1053add50ec14380f315fa887a39
Roberto Vega <roberto.vega@devopsgroup.com>
2020-05-13 16:31:00 +0100

    using type string for registry

--- site-modules/profile/manifests/registry.pp
+++ site-modules/profile/manifests/registry.pp
@@ -1,13 +1,13 @@
 class profile::registry {
   registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows NT\Reliability\ShutdownReasonOn':
     ensure => present,
-    type   => REG_DWORD,
+    type   => string,
     data   => "1",
   }
 
     registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows NT\Reliability\ShutdownReasonUI':
     ensure => present,
-    type   => REG_DWORD,
+    type   => string,
     data   => "1",
   }
 }
