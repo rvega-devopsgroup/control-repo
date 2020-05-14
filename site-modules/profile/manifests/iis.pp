b1f3642c62f6c7503457d01a79ffdbe9dae3d7b7
Roberto Vega <roberto.vega@devopsgroup.com>
2020-05-14 09:18:51 +0100

    creating a website on iis

--- site-modules/profile/manifests/iis.pp
+++ site-modules/profile/manifests/iis.pp
@@ -5,4 +5,57 @@ class profile::iis {
     ensure => present,
     answer => 'C:\answer\iis.xml',
   }
+
+  # Create Directories
+  file { 'c:\\my-website\\cats':
+    ensure  => 'directory',
+    require => Dism['IIS-WebServer'],
+  }
+
+  file { 'c:\\my-website\\cats_vdir':
+    ensure  => 'directory',
+    require => Dism['IIS-WebServer'],
+  }
+
+  file { 'c:\\my-website\\cats_vdir\\index.html':
+    ensure  => present,
+    content => 'I love cats',
+    require => File['c:\\my-website\\cats_vdir']
+  }
+
+  # Set Permissions
+  acl { 'c:\\my-website\\cats':
+    permissions => [
+      {'identity' => 'IISCompleteGroup', 'rights' => ['read', 'execute']},
+    ],
+  }
+
+  acl { 'c:\\my-website\\cats_vdir':
+    permissions => [
+      {'identity' => 'IISCompleteGroup', 'rights' => ['read', 'execute']},
+    ],
+  }
+
+  # Configure IIS
+  iis_application_pool { 'cats_app_pool':
+    ensure                  => 'present',
+    state                   => 'started',
+    managed_pipeline_mode   => 'Integrated',
+    managed_runtime_version => 'v4.0',
+  }
+
+  iis_site { 'cats':
+    ensure           => 'started',
+    physicalpath     => 'c:\\my-website\\cats',
+    applicationpool  => 'cats_app_pool',
+    enabledprotocols => 'http',
+    require          => File['c:\\my-website\\cats'],
+  }
+
+  iis_virtual_directory { 'vdir':
+    ensure       => 'present',
+    sitename     => 'complete',
+    physicalpath => 'c:\\my-website\\cats_vdir',
+    require      => File['c:\\my-website\\cats'],
+  }
 }
