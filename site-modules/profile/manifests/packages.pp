0fecf86d2e7c0788fbdcc55bc0fe9c39c8ba3ced
Roberto Vega <roberto.vega@devopsgroup.com>
2020-05-19 15:08:55 +0100

    installing 7zip using choco

--- site-modules/profile/manifests/packages.pp
+++ site-modules/profile/manifests/packages.pp
@@ -2,9 +2,16 @@
 #	  - Install 7-zip using the puppet/staging archive module - depreciated module use puppet/archive instead
 #   - Install 7-zip using chocolatey and the puppetlabs/chocolatey forge module
 class profile::packages {
+  include chocolatey
+
   class { 'archive':
     seven_zip_name     => '7-Zip 19.00 (x64 edition)',
     seven_zip_source   => 'https://www.7-zip.org/a/7z1900-x64.msi',
     seven_zip_provider => 'windows',
   }
+  # Using Chocolatey (set as default for Windows)
+  package { '7zip':
+    ensure   => 19.00,
+  }
+
 }
