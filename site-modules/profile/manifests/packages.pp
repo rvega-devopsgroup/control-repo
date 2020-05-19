beb4e2213280deb8607b2699d279e84d9777964a
Roberto Vega <roberto.vega@devopsgroup.com>
2020-05-19 11:50:35 +0100

    removing class archive

--- site-modules/profile/manifests/packages.pp
+++ site-modules/profile/manifests/packages.pp
@@ -2,9 +2,9 @@
 #	  - Install 7-zip using the puppet/staging archive module - depreciated module use puppet/archive instead
 #   - Install 7-zip using chocolatey and the puppetlabs/chocolatey forge module
 class profile::packages {
-  class { 'archive':
-    seven_zip_name     => '7-Zip 9.20 (x64 edition)',
-    seven_zip_source   => 'C:/Windows/7z920-x64.msi',
-    seven_zip_provider => 'windows',
-  }
+  # class { 'archive':
+  #   seven_zip_name     => '7-Zip 9.20 (x64 edition)',
+  #   seven_zip_source   => 'C:/Windows/7z920-x64.msi',
+  #   seven_zip_provider => 'windows',
+  # }
 }
