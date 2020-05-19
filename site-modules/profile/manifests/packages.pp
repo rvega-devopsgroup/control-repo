ee37544154353b029e43de6734bbc281c835fbe9
Roberto Vega <roberto.vega@devopsgroup.com>
2020-05-19 11:35:46 +0100

    installing 7zip using archive class

--- site-modules/profile/manifests/packages.pp
+++ site-modules/profile/manifests/packages.pp
@@ -2,7 +2,9 @@
 #	  - Install 7-zip using the puppet/staging archive module - depreciated module use puppet/archive instead
 #   - Install 7-zip using chocolatey and the puppetlabs/chocolatey forge module
 class profile::packages {
-  package { '7zip':
-    ensure => latest,
+  class { 'archive':
+    seven_zip_name     => '7-Zip 19.00 (x64 edition)',
+    seven_zip_source   => 'C:/Windows/Temp/7z1900-x64.msi',
+    seven_zip_provider => 'windows',
   }
 }
