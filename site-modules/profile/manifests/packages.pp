ef96a72b70a500add572714fccb5a04c72055e01
Roberto Vega <roberto.vega@devopsgroup.com>
2020-05-19 11:40:37 +0100

    7zip installation fails - trying an older version

--- site-modules/profile/manifests/packages.pp
+++ site-modules/profile/manifests/packages.pp
@@ -3,8 +3,8 @@
 #   - Install 7-zip using chocolatey and the puppetlabs/chocolatey forge module
 class profile::packages {
   class { 'archive':
-    seven_zip_name     => '7-Zip 19.00 (x64 edition)',
-    seven_zip_source   => 'C:/Windows/Temp/7z1900-x64.msi',
+    seven_zip_name     => '7-Zip 9.20 (x64 edition)',
+    seven_zip_source   => 'C:/Windows/Temp/7z920-x64.msi',
     seven_zip_provider => 'windows',
   }
 }
