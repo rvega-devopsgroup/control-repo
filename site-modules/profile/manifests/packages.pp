17370495751b9dad00c5bfaa9cb82390e1ddb875
Roberto Vega <roberto.vega@devopsgroup.com>
2020-05-19 11:44:24 +0100

    I think the issue might be with the temp folder permissions

--- site-modules/profile/manifests/packages.pp
+++ site-modules/profile/manifests/packages.pp
@@ -4,7 +4,7 @@
 class profile::packages {
   class { 'archive':
     seven_zip_name     => '7-Zip 9.20 (x64 edition)',
-    seven_zip_source   => 'C:/Windows/Temp/7z920-x64.msi',
+    seven_zip_source   => 'C:/Windows/7z920-x64.msi',
     seven_zip_provider => 'windows',
   }
 }
