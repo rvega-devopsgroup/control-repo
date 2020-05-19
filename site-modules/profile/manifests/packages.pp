61551b5e9e51905d434eeeda4beadd872aed9788
Roberto Vega <roberto.vega@devopsgroup.com>
2020-05-19 15:02:07 +0100

    testing archive module after installing stdlib

--- site-modules/profile/manifests/packages.pp
+++ site-modules/profile/manifests/packages.pp
@@ -2,13 +2,13 @@
 #	  - Install 7-zip using the puppet/staging archive module - depreciated module use puppet/archive instead
 #   - Install 7-zip using chocolatey and the puppetlabs/chocolatey forge module
 class profile::packages {
-  # class { 'archive':
-  #   seven_zip_name     => '7-Zip 9.20 (x64 edition)',
-  #   seven_zip_source   => 'C:/Windows/7z920-x64.msi',
-  #   seven_zip_provider => 'windows',
-  # }
-  download_file { "Download 7zip" :
-    url                   => 'https://www.7-zip.org/a/7z1900-x64.msi',
-    destination_directory => 'c:\Windows\Temp'
+  class { 'archive':
+    seven_zip_name     => '7-Zip 9.20 (x64 edition)',
+    seven_zip_source   => 'C:/Windows/7z920-x64.msi',
+    seven_zip_provider => 'windows',
   }
+  # download_file { "Download 7zip" :
+  #   url                   => 'https://www.7-zip.org/a/7z1900-x64.msi',
+  #   destination_directory => 'c:\Windows\Temp'
+  # }
 }
