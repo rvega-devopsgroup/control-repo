92b9094e1bd353097ff9137d01f97329901e23b4
Roberto Vega <roberto.vega@devopsgroup.com>
2020-05-19 11:09:43 +0100

    including archive module

--- site-modules/profile/manifests/packages.pp
+++ site-modules/profile/manifests/packages.pp
@@ -2,5 +2,5 @@
 #	  - Install 7-zip using the puppet/staging archive module - depreciated module use puppet/archive instead
 #   - Install 7-zip using chocolatey and the puppetlabs/chocolatey forge module
 class profile::packages {
-
+  include 'archive'
 }
