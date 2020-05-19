0d660003bd9fe8d8ad288f144be38aaa0a2c4ce6
Roberto Vega <roberto.vega@devopsgroup.com>
2020-05-19 16:46:28 +0100

    re-doing Windows shutdown event tracker - I think I was using the wrong key in the registry

--- site-modules/profile/manifests/registry.pp
+++ site-modules/profile/manifests/registry.pp
@@ -2,20 +2,25 @@
 #	  - Enable Internet Explorer enhanced security configuration (IEESC)
 #   - Enable the Windows Shutdown Event Tracker
 class profile::registry {
-  registry_key { 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows NT\Reliability':
-    ensure => present,
-  }
-
-  registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows NT\Reliability\ShutdownReasonOn':
-    ensure => present,
-    type   => string,
-    data   => '1',
-  }
-
-  registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows NT\Reliability\ShutdownReasonUI':
-    ensure => present,
-    type   => string,
-    data   => '1',
+  # registry_key { 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows NT\Reliability':
+  #   ensure => present,
+  # }
+  #
+  # registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows NT\Reliability\ShutdownReasonOn':
+  #   ensure => present,
+  #   type   => string,
+  #   data   => '1',
+  # }
+  #
+  # registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows NT\Reliability\ShutdownReasonUI':
+  #   ensure => present,
+  #   type   => string,
+  #   data   => '1',
+  # }
+  registry_value { 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Reliability\ShutdownReasonUI':
+    ensure   => present,
+    type     => REG_DWORD,
+    data     => '1',
   }
 
   registry_value { 'HKLM\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}\IsInstalled':
