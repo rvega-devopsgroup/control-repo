8f8c7d73f980f0f82c95f29ef5f64a5ad4cac60b
Roberto Vega <roberto.vega@devopsgroup.com>
2020-05-19 16:57:58 +0100

    I am unsure about the correct registry path - trying the old one with dword value instead string

--- site-modules/profile/manifests/registry.pp
+++ site-modules/profile/manifests/registry.pp
@@ -2,36 +2,36 @@
 #	  - Enable Internet Explorer enhanced security configuration (IEESC)
 #   - Enable the Windows Shutdown Event Tracker
 class profile::registry {
-  # registry_key { 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows NT\Reliability':
-  #   ensure => present,
-  # }
-  #
-  # registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows NT\Reliability\ShutdownReasonOn':
-  #   ensure => present,
-  #   type   => string,
-  #   data   => '1',
-  # }
-  #
-  # registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows NT\Reliability\ShutdownReasonUI':
-  #   ensure => present,
-  #   type   => string,
-  #   data   => '1',
-  # }
-  registry_value { 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Reliability\ShutdownReasonOn':
-    ensure   => present,
-    type     => dword,
-    data     => '1',
+  registry_key { 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows NT\Reliability':
+    ensure => present,
   }
 
-  registry_value { 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Reliability\ShutdownReasonUI':
-    ensure   => present,
-    type     => dword,
-    data     => '1',
+  registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows NT\Reliability\ShutdownReasonOn':
+    ensure => present,
+    type   => dword,
+    data   => '1',
   }
+
+  registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows NT\Reliability\ShutdownReasonUI':
+    ensure => present,
+    type   => dword,
+    data   => '1',
+  }
+  # registry_value { 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Reliability\ShutdownReasonOn':
+  #   ensure   => present,
+  #   type     => dword,
+  #   data     => '1',
+  # }
+  #
+  # registry_value { 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Reliability\ShutdownReasonUI':
+  #   ensure   => present,
+  #   type     => dword,
+  #   data     => '1',
+  # }
 
   registry_value { 'HKLM\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}\IsInstalled':
     ensure => present,
-    type   => string,
+    type   => dword,
     data   => '1',
   }
 }
