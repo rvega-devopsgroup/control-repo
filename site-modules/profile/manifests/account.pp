219d515494173a26ee6c383612a06516c1ad213d
Roberto Vega <roberto.vega@devopsgroup.com>
2020-05-21 10:37:16 +0100

    merging users and dir permissions into one profile

---
+++ site-modules/profile/manifests/account.pp
@@ -0,0 +1,52 @@
+# Local Accounts
+#	  - Create a local user
+#		- Create a local group
+#		- Ensure the user is in the group
+#   - Grant your user the "Log on as a Service" right
+
+# File and directory permissions
+#	  - Create a new directory
+#		  - Ensure its owner is the user you are managing
+#		  - Ensure its group similarly
+#		  - Inspect the directory permissions
+#		- Manage the directory permissions using the puppetlabs/acl module
+#		  - Grant the local user Full Control
+#     - Grant the local group Read Only permissions
+class profile::account (
+  String $username,
+) {
+  user { $username:
+    ensure     => present,
+    groups     => ['Users', 'Students'],
+    password   => '2Yz@1A!H3JkPJR9yzMcxUwpO',
+    managehome => true,
+  }
+
+  group { 'Students':
+    ensure => present,
+    before => User[$username],
+  }
+
+  # Munging failed for value "student1" in class policy_value: incomplete "\x00" on UTF-16LE
+  # local_security_policy { 'Log on as a service':
+  #   ensure         => present,
+  #   policy_value   => "student1",
+  #   require        => User['student1'],
+  # }
+
+  file { 'C:/directory':
+    ensure => 'directory',
+    owner  => 'student1',
+    group  => 'Students',
+  }
+
+  acl { 'C:/directory':
+    purge       => true,
+    permissions => [
+      { identity => 'student1', rights => ['full'] },
+      { identity => 'Students', rights => ['read'] },
+    ],
+  }
+
+
+}
--- site-modules/role/manifests/webserver.pp
+++ site-modules/role/manifests/webserver.pp
@@ -1,8 +1,7 @@
 # A Windows webserver
 class role::webserver {
   include profile::base
-  include profile::users
-  include profile::directory_permissions
+  include profile::account
   include profile::registry
   include profile::iis
   include profile::packages
